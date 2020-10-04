181de76ae652a4ff4a7083630df3166283843962
RGlendon <Rold.Glendon@gmail.com>
2020-10-04 23:08:24 +0300

    fist commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const config = require('config');
+const mongoose = require('mongoose');
+
+const app = express();
+
+const PORT = config.get('port') || 5000;
+
+
+async function start() {
+    try {
+
+    } catch (e) {
+        console.log('Server Error', e.message);
+        process.exit(1);
+    }
+}
+
+app.listen(PORT, () => console.log(`App has been started on PORT ${PORT}...`));
